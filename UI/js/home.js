64a5dd9d5f18fad534c7df33d93434e29a755dbc
Ingenio Team Gujranwala <ingenioteamgwa@gmail.com>
2020-07-27 01:30:35 +0500

    f

--- UI/js/home.js
+++ UI/js/home.js
@@ -63,7 +63,7 @@ function getBids(){
                     $('.container').append(`
                         <div class="card" style="margin-top: 5vh;">
                             <div class="card-header" style="text-align: center;">
-                                Bidder ${[i+2]^55}
+                                Bidder ${[(i+33)*55*(i+78)]}
                             </div>
                             <div class="card-body">
                             <div  class="form-group col-md-4 float-left">
